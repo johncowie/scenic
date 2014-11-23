dd70b96dcdef4e291e687b8114534a9857ca6a69
John Cowie <john.a.cowie@gmail.com>
2014-11-23 21:22:47 +0000

    Bumped snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scenic "0.2.1-SNAPSHOT"
+(defproject scenic "0.2.2-SNAPSHOT"
   :description "Bidi wrapper for pretty text file routes"
   :url "https://github.com/johncowie/scenic"
   :min-lein-version "2.0.0"
